ce899d0d8e1d92c07f0d1cd9634db42069413ff1
JohnCMello <johncharlesmello@gmail.com>
2021-08-18 13:54:11 -0300

    getDefaultProps

--- daciuk/curso_ReactJS_ninja/modulo_2/lessons_5-10/src/title.js
+++ daciuk/curso_ReactJS_ninja/modulo_2/lessons_5-10/src/title.js
@@ -3,9 +3,15 @@
 import React from 'react'
 
 const Title = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'Mussum',
+      catchPhrase: 'Cacilds'
+    }
+  },
   render: function () {
     return (
-      <h1> Hello {this.props.name} </h1>
+      <h1> Hello {this.props.name}. {this.props.catchPhrase}! </h1>
     )
   }
 })
