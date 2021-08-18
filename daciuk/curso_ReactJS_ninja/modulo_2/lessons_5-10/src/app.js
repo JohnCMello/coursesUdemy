22e1593557d4b180efc07005d615994970a83751
JohnCMello <johncharlesmello@gmail.com>
2021-08-18 19:54:23 -0300

    class components

--- daciuk/curso_ReactJS_ninja/modulo_2/lessons_5-10/src/app.js
+++ daciuk/curso_ReactJS_ninja/modulo_2/lessons_5-10/src/app.js
@@ -1,17 +1,19 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
 import Title from './title'
 
-const App = React.createClass({
-  render: function () {
+class App extends Component {
+  render() {
     return (
+      // this.props
       <div className='container'>
         <Title name='John' />
       </div>
     )
   }
+}
 
-})
+// App.defaultProps= { }
 
 export default App
