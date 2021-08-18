2dcb3eb0ad4bc39e8cbb5babbbf66beb3fee64f7
JohnCMello <johncharlesmello@gmail.com>
2021-08-18 19:32:45 -0300

    pure function components

--- daciuk/curso_ReactJS_ninja/modulo_2/lessons_5-10/src/app.js
+++ daciuk/curso_ReactJS_ninja/modulo_2/lessons_5-10/src/app.js
@@ -7,7 +7,7 @@ const App = React.createClass({
   render: function () {
     return (
       <div className='container'>
-        <Title name='John' number={0} array={[1, 2, 3]} object={{ person: 'Mussum' }} />
+        <Title name='John' />
       </div>
     )
   }
