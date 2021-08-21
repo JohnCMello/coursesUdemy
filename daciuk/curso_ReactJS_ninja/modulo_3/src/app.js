c5df6b1a0b914b287b6a1112cb8f838f3f651a21
John Mello <johncharlesmello@gmail.com>
2021-08-21 18:59:57 -0300

    add state - this.state / this.setState

--- daciuk/curso_ReactJS_ninja/modulo_3/src/app.js
+++ daciuk/curso_ReactJS_ninja/modulo_3/src/app.js
@@ -1,20 +1,24 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Square from './square'
-import Alert from './alert'
-import Button from './button'
-import LikeButton from './likeButton'
-import Searchbutton from './searchButton'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      text: 'Hello State'
+    }
+  }
+  
   render() {
     return (
-      <div className='container' onClick={() => Alert('div')}>
-        <Square />
-        <Button handleClick={Alert}> Click Me! </Button>
-        <LikeButton />
-        <Searchbutton />
+      <div 
+        className='container'  
+        onClick={() => this.setState({
+          text: 'Hello setState'
+        })}
+      >
+        {this.state.text}
       </div>
     )
   }
