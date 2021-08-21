094dd32296e0a0c720f16809b35781544c445306
JohnCMello <johncharlesmello@gmail.com>
2021-08-20 23:30:18 -0300

    makes square bigger

--- daciuk/curso_ReactJS_ninja/modulo_2/lessons_5-10/src/square.js
+++ daciuk/curso_ReactJS_ninja/modulo_2/lessons_5-10/src/square.js
@@ -2,15 +2,16 @@
 
 import React from 'react'
 
-const Square = ({ color }) => (
-  <div
-    style={{
+const Square = ({ color }) => {
+  return (
+    <div style={{
       backgroundColor: color,
       height: '200px',
       width: '200px'
     }}
-  />
-)
+    />
+  )
+}
 
 Square.defaultProps = {
   color: 'orange'
