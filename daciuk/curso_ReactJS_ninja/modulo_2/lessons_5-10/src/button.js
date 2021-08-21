f2c03f287eea234f3001e194235305ac511367f3
JohnCMello <johncharlesmello@gmail.com>
2021-08-20 23:29:53 -0300

    adds click event

--- daciuk/curso_ReactJS_ninja/modulo_2/lessons_5-10/src/button.js
+++ daciuk/curso_ReactJS_ninja/modulo_2/lessons_5-10/src/button.js
@@ -2,8 +2,18 @@
 
 import React from 'react'
 
-const Button = ({ children }) => (
-  <button>{children}</button>
+const Button = ({ children, handleClick }) => (
+  <button
+    className='main-button'
+    onClick={(e) => {
+      e.stopPropagation()
+      handleClick(children)
+    }}
+  >
+
+    {children}
+
+  </button>
 )
 
 export default Button
