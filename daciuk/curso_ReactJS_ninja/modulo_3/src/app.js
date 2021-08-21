23c39aac32033606fec47c128cff84f2a31fc34f
John Mello <johncharlesmello@gmail.com>
2021-08-21 20:59:03 -0300

    stateful X stateless components

--- daciuk/curso_ReactJS_ninja/modulo_3/src/app.js
+++ daciuk/curso_ReactJS_ninja/modulo_3/src/app.js
@@ -1,24 +1,31 @@
 'use strict'
 
 import React, { Component } from 'react'
+import Square from './square'
+import Button from './button'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      text: 'Hello State'
+      color: 'blue'
     }
   }
-  
+
   render() {
     return (
-      <div 
-        className='container'  
-        onClick={() => this.setState({
-          text: 'Hello setState'
-        })}
-      >
-        {this.state.text}
+      <div>
+        <Square color={this.state.color}/>
+
+        {['red', 'green', 'blue', 'orange', 'pink'].map(color=> (
+          <Button
+            key={color}
+            handleClick={()=> this.setState({color})}
+          > 
+            {color} 
+          </Button>
+
+        ))}
       </div>
     )
   }
