39d51576904dd38d10be849574f9d826542f9e47
JohnCMello <johncharlesmello@gmail.com>
2021-08-25 17:44:42 -0300

    propTypes

--- daciuk/curso_ReactJS_ninja/modulo_3/src/app.js
+++ daciuk/curso_ReactJS_ninja/modulo_3/src/app.js
@@ -2,61 +2,19 @@
 
 import React, { Component } from 'react'
 import Button from './button'
-import Timer from './timer'
 
 class App extends Component {
-  constructor() {
-    console.log('constructor app')
-    super()
-    this.state = {
-      time: 0,
-      showTimer: true
-    }
-  }
-
-  //deprecated - use componentDidMount
-  componentWillMount() {
-    console.log('componentWillMount app')
-  }
-
-  componentDidMount() {
-    console.log('componentDidMount app')
-  }
-
   render() {
-    console.log('inside render app')
-
     return (
       <div>
-
-        <Timer time={this.state.time} />
         <Button
-          handleClick={() => {
-            this.setState({ time: this.state.time + 1 })
-          }}
+          handleClick={() => console.log("click")}
         >
-          Change Props
+          Click Me!
         </Button>
-
       </div>
     )
   }
 }
 
-// App.defaultProps= { }
-
 export default App
-
-{
-  /* 
-  <Square color={this.state.color} />
-    {['red', 'green', 'blue', 'orange', 'pink'].map(color => (
-      <Button
-        key={color}
-        handleClick={() => this.setState({ color })}
-    >
-      {color}
-    </Button>
-  ))} 
-  */
-}
