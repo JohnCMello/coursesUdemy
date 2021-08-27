e017c1450cd0dc748de0a749ac2a9c6f00683e03
JohnCMello <johncharlesmello@gmail.com>
2021-08-27 17:50:54 -0300

    create component: Search

---
+++ daciuk/curso_ReactJS_ninja/modulo_3/githubApp/src/components/search.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import React from 'react'
+
+const Search = () => (
+  <div className='search'>
+    <input type='search' placeholder='Type user name' />
+  </div>
+)
+
+export default Search
