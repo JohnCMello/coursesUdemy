bc038debaddd8d47e4a6fb2db3ad0ca70bec9ed0
JohnCMello <johncharlesmello@gmail.com>
2021-08-30 14:35:12 -0300

    makes request on keyup event

--- daciuk/curso_ReactJS_ninja/modulo_3/githubApp/src/components/search.js
+++ daciuk/curso_ReactJS_ninja/modulo_3/githubApp/src/components/search.js
@@ -1,10 +1,25 @@
 'use strict'
 
 import React from 'react'
+import ajax from '@fdaciuk/ajax'
 
 const Search = () => (
   <div className='search'>
-    <input type='search' placeholder='Type user name' />
+    <input
+      type='search'
+      placeholder='Type user name'
+      onKeyUp={(e) => {
+        const value = e.target.value
+        const key = e.which || e.keyCode
+        const ENTER = 13
+
+        if (key === ENTER) {
+          ajax().get(`http://api.github.com/users/${value}`)
+            .then((result) => { console.log(result) })
+        }
+        console.log(key)
+      }}
+    />
   </div>
 )
 
