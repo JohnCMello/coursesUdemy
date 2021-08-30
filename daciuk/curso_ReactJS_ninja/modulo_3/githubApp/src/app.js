5d38fd619a1463e5504d7dd97b390fd7d583b2a5
JohnCMello <johncharlesmello@gmail.com>
2021-08-30 13:54:45 -0300

    add state

--- daciuk/curso_ReactJS_ninja/modulo_3/githubApp/src/app.js
+++ daciuk/curso_ReactJS_ninja/modulo_3/githubApp/src/app.js
@@ -6,12 +6,33 @@ import AppContent from './components/app-content'
 class App extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      userInfo: {
+        username: 'John Mello',
+        photo: 'https://avatars.githubusercontent.com/u/65418937?v=4',
+        login: 'johncmello',
+        repos: 10,
+        followers: 20,
+        following: 30
+      },
+      repos: [{
+        name: 'Repositories',
+        url: 'http://test1.com'
+      }],
+      starredRepos: [{
+        name: 'Starred Repositories',
+        url: 'http://test2.com'
+      }]
+    }
   }
 
   render() {
     return (
-      <AppContent />
+      <AppContent
+        userInfo={this.state.userInfo}
+        repos={this.state.repos}
+        starredRepos={this.state.starredRepos}
+      />
     )
   }
 }
