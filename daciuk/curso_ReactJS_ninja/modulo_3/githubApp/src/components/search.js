035976b38d8d099941f13201cb371dde7fea0d76
JohnCMello <johncharlesmello@gmail.com>
2021-09-02 17:36:56 -0300

    add handleSearch onkeyup event

--- daciuk/curso_ReactJS_ninja/modulo_3/githubApp/src/components/search.js
+++ daciuk/curso_ReactJS_ninja/modulo_3/githubApp/src/components/search.js
@@ -1,26 +1,19 @@
 'use strict'
 
-import React from 'react'
-import ajax from '@fdaciuk/ajax'
+import React, { PropTypes } from 'react'
 
-const Search = () => (
+const Search = ({ handleSearch }) => (
   <div className='search'>
     <input
       type='search'
       placeholder='Type user name'
-      onKeyUp={(e) => {
-        const value = e.target.value
-        const key = e.which || e.keyCode
-        const ENTER = 13
-
-        if (key === ENTER) {
-          ajax().get(`http://api.github.com/users/${value}`)
-            .then((result) => { console.log(result) })
-        }
-        console.log(key)
-      }}
+      onKeyUp={handleSearch}
     />
   </div>
 )
+
+Search.propTypes = {
+  handleSearch: PropTypes.func.isRequired
+}
 
 export default Search
