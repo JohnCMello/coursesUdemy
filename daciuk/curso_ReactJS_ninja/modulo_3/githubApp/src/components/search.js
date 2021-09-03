aaf853d7fbe1a714ae87de9e5b7e05fe350ab1b1
JohnCMello <johncharlesmello@gmail.com>
2021-09-03 18:06:56 -0300

    add proptypes

--- daciuk/curso_ReactJS_ninja/modulo_3/githubApp/src/components/search.js
+++ daciuk/curso_ReactJS_ninja/modulo_3/githubApp/src/components/search.js
@@ -2,18 +2,20 @@
 
 import React, { PropTypes } from 'react'
 
-const Search = ({ handleSearch }) => (
+const Search = ({ handleSearch, isSearchDisabled }) => (
   <div className='search'>
     <input
       type='search'
       placeholder='Type user name'
       onKeyUp={handleSearch}
+      disabled={isSearchDisabled}
     />
   </div>
 )
 
 Search.propTypes = {
-  handleSearch: PropTypes.func.isRequired
+  handleSearch: PropTypes.func.isRequired,
+  isSearchDisabled: PropTypes.bool.isRequired
 }
 
 export default Search
