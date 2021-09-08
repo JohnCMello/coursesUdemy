79843f6cab241516645ac48af2e08ccbbaaab5d2
JohnCMello <johncharlesmello@gmail.com>
2021-09-08 12:46:48 -0300

    a short intro to TDD

--- daciuk/curso_ReactJS_ninja/modulo_3/tests/test/sum-test.js
+++ daciuk/curso_ReactJS_ninja/modulo_3/tests/test/sum-test.js
@@ -2,4 +2,19 @@
 
 const sum = require('./sum')
 
-console.log(sum(3, 4))
+console.assert(
+  typeof sum === 'function',
+  'shum should be a function'
+)
+
+console.assert(
+  sum(1, 2) === 3,
+  'sum(1,2) should return 3'
+)
+
+console.assert(
+  sum(2, 3) === 5,
+  'sum(2,3) should return 5'
+)
+
+console.log('everything is working fine!')
