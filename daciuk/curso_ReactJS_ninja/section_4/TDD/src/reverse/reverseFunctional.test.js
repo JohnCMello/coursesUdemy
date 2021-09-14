f5fc64fc868ac00d587740dc88c4d9b54fbdf87c
JohnCMello <johncharlesmello@gmail.com>
2021-09-14 19:31:18 -0300

    TDD - reverse recursive

---
+++ daciuk/curso_ReactJS_ninja/section_4/TDD/src/reverse/reverseFunctional.test.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import { expect } from "chai"
+import reverse from './reverseFunctional'
+
+it('reverse should be a function', () => {
+  expect(reverse).to.be.a('function')
+})
+
+it('reverse([2], (item) => item) should return [2]', () => {
+  expect(reverse([2]), (item) => item).to.be.deep.equal([2])
+})
+
+it('reverse([2,1], (item) => item) should return [1,2]', () => {
+  expect(reverse([2, 1]), (item) => item).to.be.deep.equal([1, 2])
+})
