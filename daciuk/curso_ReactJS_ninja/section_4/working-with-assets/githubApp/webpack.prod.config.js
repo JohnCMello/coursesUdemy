c9f7ee1b4c774ddace51979ee3e48b0e632b8eb8
JohnCMello <johncharlesmello@gmail.com>
2021-09-23 00:05:22 -0300

    critical rendering path

--- daciuk/curso_ReactJS_ninja/section_4/working-with-assets/githubApp/webpack.prod.config.js
+++ daciuk/curso_ReactJS_ninja/section_4/working-with-assets/githubApp/webpack.prod.config.js
@@ -6,6 +6,9 @@ const webpack = require('webpack')
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
+const crp = new ExtractTextPlugin('crp.css')
+const styles = new ExtractTextPlugin('[name]-[hash].css')
+
 module.exports = {
   entry: path.join(__dirname, 'src', 'index'),
 
@@ -15,7 +18,9 @@ module.exports = {
   },
 
   plugins: [
-    new ExtractTextPlugin('[name]-[hash].css'),
+
+    crp,
+    styles,
 
     new webpack.DefinePlugin({
       'process.env': {
@@ -28,35 +33,45 @@ module.exports = {
     }),
 
     new webpack.optimize.DedupePlugin(),
-
     new webpack.optimize.OccurrenceOrderPlugin(),
 
     new HtmlPlugin({
       title: 'GitHub App',
+      inject: false,
       template: path.join(__dirname, 'src', 'html', 'template.html')
     })
   ],
 
   module: {
-    preLoaders: [{
-      test: /.js$/,
-      exclude: /node_modules/,
-      include: /src/,
-      loader: 'standard'
+    preLoaders: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        include: /src/,
+        loader: 'standard'
+      }
+    ],
 
-    }],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: /src/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules|(search|style)\.css/,
+        include: /src/,
+        loader: styles.extract('style', 'css')
+      },
+      {
+        test: /(search|style)\.css$/,
+        exclude: /node_modules/,
+        include: /src/,
+        loader: crp.extract('style', 'css')
+      }
 
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      include: /src/,
-      loader: 'babel-loader'
-    },
-    {
-      test: /\.css$/,
-      exclude: /node_modules/,
-      include: /src/,
-      loader: ExtractTextPlugin.extract('style', 'css')
-    }]
+    ]
   }
 }
