7c1137e2d4eaed28822dc5b905b5fc70d8414a90
JohnCMello <johncharlesmello@gmail.com>
2021-11-18 18:34:38 -0300

    section 10

--- lomiranda/secao-10/express/src/middlewares/middleware.js
+++ lomiranda/secao-10/express/src/middlewares/middleware.js
@@ -2,3 +2,9 @@ exports.myMiddleware = (req, res, next) => {
   res.locals.localVariable = 'testing local variable'
   next()
 }
+
+exports.checkCSRFError = (err, req, res, next) => {
+  if (err && err.code === 'EBADCSRFTOKEN') {
+    return res.send()
+  }
+}
