1b1ad22de9e5400ca0190a4b1af79ac481310cee
JohnCMello <johncharlesmello@gmail.com>
2021-12-10 14:57:52 -0300

    home controller

--- lomiranda/secao-11/src/controllers/homeController.js
+++ lomiranda/secao-11/src/controllers/homeController.js
@@ -1,10 +1,3 @@
-exports.handleGet = (req, res) => {
-  res.render('index', {
-    title: 'Some Title',
-    numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  })
-}
-
-exports.handlePost = (req, res) => {
-  res.send(req.body)
+exports.index = (req, res) => {
+  res.render('index')
 }
