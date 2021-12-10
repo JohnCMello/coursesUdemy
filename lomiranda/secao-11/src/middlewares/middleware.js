8725d1a0592b3c82d4a3698238493cde6cddbe62
JohnCMello <johncharlesmello@gmail.com>
2021-12-10 14:59:11 -0300

    check is user logged - check crsf token/errors

--- lomiranda/secao-11/src/middlewares/middleware.js
+++ lomiranda/secao-11/src/middlewares/middleware.js
@@ -1,10 +1,28 @@
 exports.myMiddleware = (req, res, next) => {
-  res.locals.localVariable = 'testing local variable'
-  next()
-}
+  res.locals.errors = req.flash('errors');
+  res.locals.success = req.flash('success');
+  res.locals.user = req.session.user;
+  next();
+};
 
 exports.checkCSRFError = (err, req, res, next) => {
-  if (err && err.code === 'EBADCSRFTOKEN') {
-    return res.send()
+  if (err) {
+    return res.render('404');
   }
-}
+  next();
+};
+
+exports.csrfMiddleware = (req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+};
+
+exports.loginRequired = (req, res, next) => {
+  if (!req.session.user) {
+    req.flash('errors', 'You must log in.');
+    req.session.save(() => res.redirect('/'));
+    return;
+  }
+
+  next();
+};
