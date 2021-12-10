c973f76d8c680bc107166aab97e2884e9ef5eb07
JohnCMello <johncharlesmello@gmail.com>
2021-12-10 14:57:04 -0300

    remove test css

--- lomiranda/secao-11/webpack.config.js
+++ lomiranda/secao-11/webpack.config.js
@@ -8,21 +8,16 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    rules: [
-      {
-        exclude: /node_modules/,
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/env']
-          }
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
         }
-      }, {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
       }
-    ]
+    }]
   },
   devtool: 'source-map'
 };
