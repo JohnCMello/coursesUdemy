6e44132807e89c79e2aeba0e0d615fd1a05ccfd3
JohnCMello <johncharlesmello@gmail.com>
2022-04-07 17:11:40 -0300

    counter working with redux

--- daciuk/curso_ReactJS_ninja/section_5/redux/main.js
+++ daciuk/curso_ReactJS_ninja/section_5/redux/main.js
@@ -11,15 +11,34 @@ const counter = (state = 0, action) => {
 const { createStore } = Redux;
 const store = createStore(counter);
 
-console.log('store.getState', store.getState())
+// NOT USING STORE
+//                      $counter.innerText = +$counter.innerText + 1
+const increment = () => store.dispatch({ type: 'INCREMENT' });
+//                      $counter.innerText = +$counter.innerText - 1
+const decrement = () => store.dispatch({ type: 'DECREMENT' });
+
+const $counter = document.querySelector('[data-js="counter"]');
+const $decrement = document.querySelector('[data-js="decrement"]');
+const $increment = document.querySelector('[data-js="increment"]');
+
+$decrement.addEventListener('click', decrement, false);
+$increment.addEventListener('click', increment, false);
+
+
 store.subscribe(() => {
+  $counter.innerText = store.getState()
   console.log('store.dispatch was called, an action happened - STATE:', store.getState())
 })
-store.dispatch({ type: 'INCREMENT' })
-console.log('store.getState', store.getState())
-store.dispatch({ type: 'DECREMENT' })
-store.dispatch({ type: 'DECREMENT' })
-store.dispatch({ type: 'DECREMENT' })
-store.dispatch({ type: 'DECREMENT' })
-console.log('store.getState', store.getState())
-store.dispatch({ type: 'INCREMENT' })
+
+
+
+// console.log('store.getState', store.getState())
+// store.dispatch({ type: 'INCREMENT' })
+// console.log('store.getState', store.getState())
+// store.dispatch({ type: 'DECREMENT' })
+// store.dispatch({ type: 'DECREMENT' })
+// store.dispatch({ type: 'DECREMENT' })
+// store.dispatch({ type: 'DECREMENT' })
+// console.log('store.getState', store.getState())
+// store.dispatch({ type: 'INCREMENT' })
+
