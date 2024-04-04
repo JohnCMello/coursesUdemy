fad5a7c4eead218264e51518e83f3f8febb457c4
JohnCMello <johncharlesmello@gmail.com>
2024-04-04 16:14:08 -0300

    fim seção 5

---
+++ curso_java_nelio_alves/src/estruturaCondicional/ExpressoesComparativas.java
@@ -0,0 +1,19 @@
+package estruturaCondicional;
+
+public class ExpressoesComparativas {
+
+	public static void main(String[] args) {
+	/*
+	 * Operadores
+	 * 
+	 * < menor
+	 * > maior
+	 * <= menor igual
+	 * >= maior igual
+	 * == igual
+	 * != diferente
+	 */
+
+	}
+
+}
