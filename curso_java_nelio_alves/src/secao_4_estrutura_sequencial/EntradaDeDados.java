d5933722055382f65bc05628e0943b2fddc49c52
JohnCMello <johncharlesmello@gmail.com>
2024-04-04 15:15:23 -0300

    fim seção 4

---
+++ curso_java_nelio_alves/src/secao_4_estrutura_sequencial/EntradaDeDados.java
@@ -0,0 +1,54 @@
+package secao_4_estrutura_sequencial;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EntradaDeDados {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+//		String x = sc.next();
+
+//		int x = sc.nextInt();	
+
+//		digitar numero flutuante com pontuação diferente do locale lança erro.
+//		digitar 4.5 com locale do brasil da erro, pois no Brasil se usa , (virgula) e vice-versa.
+//		double x = sc.nextDouble();
+
+//		char x = sc.next().charAt(0);
+
+//		System.out.println("Você digitou " + x);
+
+//		String x = sc.next();
+//		int y = sc.nextInt();
+//		double z = sc.nextDouble();
+
+//		System.out.println(x);
+//		System.out.println(y);
+//		System.out.println(z);
+		
+		
+		int x;
+		String s1, s2, s3;
+		
+		x = sc.nextInt();
+//		com nextInt existe uma quebra de linha pendente que é consuimida pelo proximo nextLine.
+//		nesse caso o proximo nextLine inicia com uma string vazia, s1 recebe uma string vazia.
+//		para resolver podemos adicionar um novo nextLine que vai consumir a quebra de linha do nextInt anterior.
+		sc.nextLine();
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+		
+		System.out.println("DADOS DIGITADOS:");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+
+		sc.close();
+	}
+
+}
---
+++ curso_java_nelio_alves/src/secao_4_estrutura_sequencial/Variaveis.java
@@ -0,0 +1,74 @@
+package secao_4_estrutura_sequencial;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+//		<tipo> <nome> = <valor inicial>;
+
+		int idade = 25;
+		double altura = 1.85;
+		char sexo = 'F';
+
+		/*
+		 * tipos primitivos
+		 * 
+		 * Um bit é a menor unidade de informação em um sistema de computador. Ele pode
+		 * armazenar apenas um valor, que pode ser 0 ou 1. Esse valor binário pode
+		 * representar diversas coisas, como um estado ligado/desligado,
+		 * verdadeiro/falso, ou qualquer outra interpretação que atribuímos a ele no
+		 * contexto de um programa ou sistema computacional. Essencialmente, os bits são
+		 * os blocos básicos de construção de toda a informação e operações que ocorrem
+		 * em um computador.
+		 * 
+		 * NUMERICOS INTEIROS 
+		 * byte: 
+		 * Tamanho: 8 bits 
+		 * Valores: -128 a 127 
+		 * Valor padrão: 0
+		 * 
+		 * short: 
+		 * Tamanho: 16 bits 
+		 * Valores: -32,768 a 32,767 
+		 * Valor padrão: 0
+		 * 
+		 * int: 
+		 * Tamanho: 32 bits 
+		 * Valores: -2,147,483,648 a 2,147,483,647 
+		 * Valor padrão: 0
+		 * 
+		 * long: 
+		 * Tamanho: 64 bits 
+		 * Valores: -9,223,372,036,854,775,808 a 9,223,372,036,854,775,807 
+		 * Valor padrão: 0L
+		 * 
+		 * 
+		 * NUMERICOS COM PONTO FLUTUANTE
+		 * float: 
+		 * Tamanho: 32 bits 
+		 * Valores: Aproximadamente ±3.40282347E+38F 
+		 * Valor padrão: 0.0f
+		 * 
+		 * double: 
+		 * Tamanho: 64 bits 
+		 * Valores: Aproximadamente ±1.79769313486231570E+308
+		 * Valor padrão: 0.0d
+		 * 
+		 * 
+		 * CARACTERE UNICODE
+		 * char: 
+		 * Tamanho: 16 bits 
+		 * Valores: 0 a 65,535 
+		 * Valor padrão: '\u0000' (nulo)
+		 * 
+		 * 
+		 * VALOR VERDADE
+		 * boolean: 
+		 * Tamanho: Depende da implementação 
+		 * Valores: true ou false 
+		 * Valor padrão: false
+		 * 
+		 */
+
+	}
+
+}
